0c025fc368c283f53f47ee01c441f18f8ca27879
Niek Willems <niek@Nieks-Mac.local>
2014-04-16 03:49:20 -0700

    Project aangemaakt

---
+++ Glassy/Glassy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Glassy
+//
+//  Created by Niek Willems on 16/04/14.
+//  Copyright (c) 2014 InnoYou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Glassy/GlassyTests/GlassyTests.m
@@ -0,0 +1,34 @@
+//
+//  GlassyTests.m
+//  GlassyTests
+//
+//  Created by Niek Willems on 16/04/14.
+//  Copyright (c) 2014 InnoYou. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GlassyTests : XCTestCase
+
+@end
+
+@implementation GlassyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
