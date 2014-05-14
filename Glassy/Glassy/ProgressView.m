cccfe0f757423a2bb8d9e0fb7130dd1d3948f23b
T. Slot <tim@Tims-Mac.local>
2014-05-14 12:23:46 -0700

    stappen view dynamic mockup

--- Glassy/Glassy/ProgressView.m
+++ Glassy/Glassy/ProgressView.m
@@ -22,9 +22,9 @@
 - (void)drawView
 {
     NSInteger progressbarSize = 300;
-    NSInteger mediaviewHeight = 0;
     CGFloat currentHeight = 0;
     CGFloat margin = 5;
+    NSInteger mediaviewHeight = margin;
     CGFloat frameWidth = [[UIScreen mainScreen] bounds].size.width;
     
     self.backgroundColor = [UIColor clearColor];
@@ -38,7 +38,7 @@
     
     currentHeight += self.titleLabel.frame.size.height + margin;
     
-    self.mediaview = [[UIView alloc]initWithFrame:CGRectMake(margin, currentHeight, frameWidth - margin * 2, 150 + margin * 4)];
+    self.mediaview = [[UIView alloc]initWithFrame:CGRectMake(margin, currentHeight, frameWidth - margin * 2, 170 + margin * 4)];
     self.mediaview.backgroundColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.5];
     self.mediaview.layer.borderColor = [UIColor colorWithRed:255.0 green:255.0 blue:255.0 alpha:0.5].CGColor;
     self.mediaview.layer.borderWidth = 1.0;
@@ -52,7 +52,7 @@
     // Set verzamelen
     self.verzamelenLbl = [[UILabel alloc] initWithFrame:CGRectMake(margin, mediaviewHeight, frameWidth, 24.0f)];
     self.verzamelenLbl.text = @"1. Bewoners verzamelen";
-    self.verzamelenLbl.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:20.0f];
+    self.verzamelenLbl.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f];
 
     self.verzamelenLbl.textColor = [UIColor whiteColor];
     [self.mediaview addSubview:self.verzamelenLbl];
@@ -60,8 +60,8 @@
     mediaviewHeight += self.verzamelenLbl.frame.size.height + margin;
     currentHeight += self.verzamelenLbl.frame.size.height + margin;
     
-    self.inschrijvenProgress = [[UIProgressView alloc] initWithFrame:CGRectMake(margin, 45, progressbarSize, 24.0f)];
-    self.inschrijvenProgress.progress = 0.75f;
+    self.inschrijvenProgress = [[UIProgressView alloc] initWithFrame:CGRectMake(margin, mediaviewHeight, progressbarSize, 24.0f)];
+    self.inschrijvenProgress.progress = 0.95f;
     [self.mediaview addSubview:self.inschrijvenProgress];
 
     mediaviewHeight += self.verzamelenProgress.frame.size.height + margin;
@@ -70,7 +70,7 @@
     // Set inschrijven
     self.inschrijvenLbl = [[UILabel alloc] initWithFrame:CGRectMake(margin, mediaviewHeight, frameWidth, 24.0f)];
     self.inschrijvenLbl.text = @"2. Inschrijven";
-    self.inschrijvenLbl.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:20.0f];
+    self.inschrijvenLbl.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f];
     
     self.inschrijvenLbl.textColor = [UIColor whiteColor];
     [self.mediaview addSubview:self.inschrijvenLbl];
@@ -78,13 +78,66 @@
     mediaviewHeight += self.inschrijvenLbl.frame.size.height + margin;
     currentHeight += self.inschrijvenLbl.frame.size.height + margin;
     
-    self.inschrijvenProgress = [[UIProgressView alloc] initWithFrame:CGRectMake(margin, 45, progressbarSize, 24.0f)];
+    self.inschrijvenProgress = [[UIProgressView alloc] initWithFrame:CGRectMake(margin, mediaviewHeight, progressbarSize, 24.0f)];
     self.inschrijvenProgress.progress = 0.75f;
     [self.mediaview addSubview:self.inschrijvenProgress];
     
     mediaviewHeight += self.inschrijvenProgress.frame.size.height + margin;
     currentHeight += self.inschrijvenProgress.frame.size.height + margin;
     
+    // Set provider kiezen
+    self.providerLbl = [[UILabel alloc] initWithFrame:CGRectMake(margin, mediaviewHeight, frameWidth, 24.0f)];
+    self.providerLbl.text = @"3. Provider kiezen";
+    self.providerLbl.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f];
+    
+    self.providerLbl.textColor = [UIColor whiteColor];
+    [self.mediaview addSubview:self.providerLbl];
+    
+    mediaviewHeight += self.providerLbl.frame.size.height + margin;
+    currentHeight += self.providerLbl.frame.size.height + margin;
+    
+    self.providerProgress = [[UIProgressView alloc] initWithFrame:CGRectMake(margin, mediaviewHeight, progressbarSize, 24.0f)];
+    self.providerProgress.progress = 0.65f;
+    [self.mediaview addSubview:self.providerProgress];
+    
+    mediaviewHeight += self.providerProgress.frame.size.height + margin;
+    currentHeight += self.providerProgress.frame.size.height + margin;
+    
+    // Set aanleg
+    self.aanlegLbl = [[UILabel alloc] initWithFrame:CGRectMake(margin, mediaviewHeight, frameWidth, 24.0f)];
+    self.aanlegLbl.text = @"4. Glasvezel aanleggen";
+    self.aanlegLbl.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f];
+    
+    self.aanlegLbl.textColor = [UIColor whiteColor];
+    [self.mediaview addSubview:self.aanlegLbl];
+    
+    mediaviewHeight += self.aanlegLbl.frame.size.height + margin;
+    currentHeight += self.aanlegLbl.frame.size.height + margin;
+    
+    self.aanlegProgress = [[UIProgressView alloc] initWithFrame:CGRectMake(margin, mediaviewHeight, progressbarSize, 24.0f)];
+    self.aanlegProgress.progress = 0.25f;
+    [self.mediaview addSubview:self.aanlegProgress];
+    
+    mediaviewHeight += self.aanlegProgress.frame.size.height + margin;
+    currentHeight += self.aanlegProgress.frame.size.height + margin;
+    
+    // Set overtappen
+    self.overstappenLbl = [[UILabel alloc] initWithFrame:CGRectMake(margin, mediaviewHeight, frameWidth, 24.0f)];
+    self.overstappenLbl.text = @"5. Overstappen naar glasvezel";
+    self.overstappenLbl.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18.0f];
+    
+    self.overstappenLbl.textColor = [UIColor whiteColor];
+    [self.mediaview addSubview:self.overstappenLbl];
+    
+    mediaviewHeight += self.overstappenLbl.frame.size.height + margin;
+    currentHeight += self.overstappenLbl.frame.size.height + margin;
+    
+    self.overstappenProgress = [[UIProgressView alloc] initWithFrame:CGRectMake(margin, mediaviewHeight, progressbarSize, 24.0f)];
+    self.overstappenProgress.progress = 0.2f;
+    [self.mediaview addSubview:self.overstappenProgress];
+    
+    mediaviewHeight += self.overstappenProgress.frame.size.height + margin;
+    currentHeight += self.overstappenProgress.frame.size.height + margin;
     /* --------------- TEMP */
     //UIImage *background = [UIImage imageNamed:@"ios-progress.png"];
     //UIImageView *imageView = [[UIImageView alloc] initWithImage:background];
